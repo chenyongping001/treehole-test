82a978de1c10e8c4c11dfececc3c8a889e7be4b6
chenyongping <chenyongping001@gmail.com>
2021-09-26 17:00:06 +0800

    initial commit

--- pages/enroll/enroll.js
+++ pages/enroll/enroll.js
@@ -5,16 +5,97 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    username: "",
+    phonenumber: "",
+    password: "",
+    passwordAck: "",
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-
+  onLoad: function (options) {},
+  usernameInput: function (e) {
+    this.setData({
+      username: e.detail.value
+    })
   },
-
+  phonenumberInput: function (e) {
+    this.setData({
+      phonenumber: e.detail.value
+    })
+  },
+  passwordInput: function (e) {
+    this.setData({
+      password: e.detail.value
+    })
+  },
+  passwordAckInput: function (e) {
+    this.setData({
+      passwordAck: e.detail.value
+    })
+  },
+  signin: function (e) {
+    wx.navigateBack({
+      delta: 1,
+    })
+  },
+  regist: function (e) {
+    var that = this
+    var myreg = /^(((13[0-9]{1})|(15[0-9]{1})|(18[0-9]{1})|(17[0-9]{1}))+\d{8})$/;
+    if (that.data.username === '') {
+      wx.showModal({
+        title: '提示',
+        content: '请输入用户名',
+        showCancel: false,
+        success(res) {}
+      })
+    } else if (that.data.phonenumber === '') {
+      wx.showModal({
+        title: '提示',
+        content: '请输入手机号',
+        showCancel: false,
+        success(res) {}
+      })
+    } else if (that.data.phonenumber.length !== 11) {
+      wx.showModal({
+        title: '提示',
+        content: '手机长度有误，请重新输入！',
+        showCancel: false,
+        success(res) {}
+      })
+    } else if (!myreg.test(that.data.phonenumber)) {
+      wx.showModal({
+        title: '提示',
+        content: '请输入正确的手机号码！',
+        showCancel: false,
+        success(res) {}
+      })
+    } else if (that.data.password === '') {
+      wx.showModal({
+        title: '提示',
+        content: '请输入密码',
+        showCancel: false,
+        success(res) {}
+      })
+    } else if (that.data.passwordAck === '') {
+      wx.showModal({
+        title: '提示',
+        content: '请确认密码',
+        showCancel: false,
+        success(res) {}
+      })
+    }else if (that.data.passwordAck !==that.data.password) {
+      wx.showModal({
+        title: '提示',
+        content: '两次输入密码不一致！',
+        showCancel: false,
+        success(res) {}
+      })
+    } else {
+      console.log("success")
+    }
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
--- pages/load/load.js
+++ pages/load/load.js
@@ -5,14 +5,69 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    userInfo: {},
+    hasUserInfo: false,
+    canIUseGetUserProfile: false,
   },
-
+  next: function (e) {
+    console.log("userInfo", getApp().globalData.userInfo)
+    wx.redirectTo({
+      url: '/pages/login/login'
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    var that = this
+    wx.showLoading({
+      title: '加载中',
+    })
+    if (wx.getUserProfile) {
+      this.setData({
+        canIUseGetUserProfile: true
+      })
+    }
+    if (!this.data.hasUserInfo) {
+      wx.login({
+        success(res) {
+          if (res.code) {
 
+          } else {
+            console.log('登录失败！' + res.errMsg)
+          }
+        }
+      })
+    }else{
+      this.next()
+    }
+
+
+    setTimeout(() => {
+      wx.hideLoading()
+    }, 2000);
+  },
+  getUserProfile(e) {
+    // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认
+    // 开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
+    wx.getUserProfile({
+      desc: '用于完善会员资料', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
+      success: (res) => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+        getApp().globalData.userInfo=res.userInfo
+        this.next()
+      }
+    })
+  },
+  getUserInfo(e) {
+    // 不推荐使用getUserInfo获取用户信息，预计自2021年4月13日起，getUserInfo将不再弹出弹窗，并直接返回匿名的用户个人信息
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
   },
 
   /**
