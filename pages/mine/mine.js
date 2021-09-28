0e3007967154ea7b362d2c67ad51c320e1fe802d
chenyongping <chenyongping001@gmail.com>
2021-09-28 11:14:56 +0800

    mine treehole

--- pages/mine/mine.js
+++ pages/mine/mine.js
@@ -5,9 +5,47 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    fircol:"#979797",
+    seccol:"#000000",
+    face_url:"/images/likeplus.png",
+    username:"哆啦B梦",
+    list:[
+      {
+        send_timestamp:"2021-9-27 17:09:12",
+        content:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea aspernatur cupiditate quas fugiat vel sed incidunt, id sint aperiam aliquid obcaecati, porro magni. Tenetur quos facere excepturi natus iure quia!",
+        total_likes:99,
+      }, 
+      {
+        send_timestamp:"2021-9-27 17:20:12",
+        content:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea aspernatur cupiditate quas fugiat vel sed incidunt, id sint aperiam aliquid obcaecati, porro magni. Tenetur quos facere excepturi natus iure quia!",
+        total_likes:95,
+      }, 
+    ],
   },
-
+  first_select:function(e){
+    wx.redirectTo({
+      url: '/pages/square/square',
+    })
+  },
+  second_select:function(e){
+    wx.navigateTo({
+      url: '/pages/commit/commit',
+    })
+  },
+  third_select:function(e){},
+  delete:function(e){
+    wx.showModal({
+      title: '',
+      content: '是否确认删除?',
+      success (res) {
+        if (res.confirm) {
+          console.log('用户点击确定')
+        } else if (res.cancel) {
+          console.log('用户点击取消')
+        }
+      }
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
